9bd05c88237a1d9cb8abfbfecdb5da13ac44d795
Mohamed Salah <mohamed.salah@badrit.com>
2013-10-24 14:42:49 +0200

    fix java file

--- src/android/PrintPlugin.java
+++ src/android/PrintPlugin.java
@@ -3,8 +3,10 @@ package com.phonegap.plugins;
 import java.io.File;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
+
 import android.content.Intent;
 import android.net.Uri;
 
@@ -12,24 +14,33 @@ import android.net.Uri;
  * The Class MacAddressPlugin.
  */
 public class PrintPlugin extends CordovaPlugin {
+	private CallbackContext callbackContext;
 
 	@Override
 	public boolean execute(String action, JSONArray args,
 			CallbackContext callbackContext) throws JSONException {
+		this.callbackContext = callbackContext;
 		if (action.equals("print")) {
 			String uri = args.getString(0);
 			String type = args.getString(1);
 			String title = args.getString(2);
 			Intent printIntent = new Intent(this.cordova.getActivity(),
 					PrintDialogActivity.class);
-			printIntent.setDataAndType(
-					Uri.fromFile(new File(uri)),
-					type);
+			printIntent.setDataAndType(Uri.fromFile(new File(uri)), type);
 			printIntent.putExtra("title", title);
-			cordova.startActivityForResult(this, printIntent, 1);
+			this.cordova.startActivityForResult(this, printIntent, 1);
+			PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
+			r.setKeepCallback(true);
+			callbackContext.sendPluginResult(r);
 			return true;
 		}
+		callbackContext.error(0);
 		return false;
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		callbackContext.success();
+
 	}
 
 }
--- www/PrintPlugin.js
+++ www/PrintPlugin.js
@@ -1,10 +1,15 @@
 var PrintPlugin = {
 
 	print: function(printHTML, successCallback, errorCallback, uri, type, title) {
-		if (device.platform == "Android") 
+		if (device.platform == "Android") {
+			alert('android');
 			cordova.exec(successCallback, errorCallback, "PrintPlugin", "print", [uri, type, title]);
+		}
 		else
+		{
+			alert('ios');
 			cordova.exec(successCallback, errorCallback, 'PrintPlugin', 'print', [printHTML]);
+		}
 	},
 	isPrintingAvailable: function(successCallback, errorCallback){
 		cordova.exec(successCallback, errorCallback, 'PrintPlugin', 'isPrintingAvailable', []);
