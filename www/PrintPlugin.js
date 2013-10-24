79394c2d822b174fea4efc84f8af7b30ac85f8bf
Mohamed Salah <mohamed.salah@badrit.com>
2013-10-24 13:57:06 +0200

    add ios plugin

--- www/PrintPlugin.js
+++ www/PrintPlugin.js
@@ -1,7 +1,13 @@
 var PrintPlugin = {
 
-	print: function(success, fail, uri, type, title) {
-		cordova.exec(success, fail, "PrintPlugin", "print", [uri, type, title]);
+	print: function(printHTML, successCallback, errorCallback, uri, type, title) {
+		if (device.platform == "Android") 
+			cordova.exec(successCallback, errorCallback, "PrintPlugin", "print", [uri, type, title]);
+		else
+			cordova.exec(successCallback, errorCallback, 'PrintPlugin', 'print', [printHTML]);
+	},
+	isPrintingAvailable: function(successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'PrintPlugin', 'isPrintingAvailable', []);
 	}
 };
 
