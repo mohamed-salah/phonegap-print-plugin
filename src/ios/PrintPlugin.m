b2d5dbca465be6196408f34615851bb774529084
Hazem Hagrass <hazem.hagrass@badrit.com>
2013-10-31 15:06:55 +0200

    Fix printing images and add header to printing pages

--- src/ios/PrintPlugin.m
+++ src/ios/PrintPlugin.m
@@ -22,9 +22,9 @@
 /*
  Is printing available. Callback returns true/false if printing is available/unavailable.
  */
-- (void) isPrintingAvailable:(CDVInvokedUrlCommand*)command{
+ - (void) isPrintingAvailable:(CDVInvokedUrlCommand*)command{
     NSUInteger argc = [command.arguments count];
-
+    
     if (argc < 1) {
         return;
     }
@@ -39,19 +39,19 @@
         return;
     }
     self.printHTML = [command.arguments objectAtIndex:0];
-
+    
     if (![self isPrintServiceAvailable]){
         [self callbackWithFuntion:self.failCallback withData: @"{success: false, available: false}"];
-
+        
         return;
     }
-
+    
     UIPrintInteractionController *controller = [UIPrintInteractionController sharedPrintController];
-
+    
     if (!controller){
         return;
     }
-
+    
     if ([UIPrintInteractionController isPrintingAvailable]){
         //Set the priner settings
         UIPrintInfo *printInfo = [UIPrintInfo printInfo];
@@ -61,16 +61,16 @@
         }
         controller.printInfo = printInfo;
         controller.showsPageRange = YES;
-
-
+        
+        
         //Set the base URL to be the www directory.
         NSString *dbFilePath = [[NSBundle mainBundle] pathForResource:@"www" ofType:nil ];
         NSURL *baseURL = [NSURL fileURLWithPath:dbFilePath];
-
+        
         //Load page into a webview and use its formatter to print the page
         UIWebView *webViewPrint = [[UIWebView alloc] init];
         [webViewPrint loadHTMLString:printHTML baseURL:baseURL];
-
+        
         //Get formatter for web (note: margin not required - done in web page)
         UIViewPrintFormatter *viewFormatter = [webViewPrint viewPrintFormatter];
         if(argc >= 3) {
@@ -79,10 +79,15 @@
         if(argc >= 4) {
             viewFormatter.maximumContentHeight = [[command.arguments objectAtIndex:3] intValue];
         }
-        controller.printFormatter = viewFormatter;
+        NSError *error = nil;
+        controller.printFormatter = [[UIMarkupTextPrintFormatter alloc] initWithMarkupText:self.printHTML];
+        if (error) {
+            NSLog(@"Error while creating the print formatter:\n%@", error);
+            NSLog(@"%@", dbFilePath);
+        }
         controller.showsPageRange = YES;
-
-
+        
+        
         void (^completionHandler)(UIPrintInteractionController *, BOOL, NSError *) =
         ^(UIPrintInteractionController *printController, BOOL completed, NSError *error) {
             CDVPluginResult* pluginResult = nil;
@@ -94,29 +99,30 @@
             }
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         };
-
+        
         /*
-         If iPad, and if button offsets passed, then show dilalog originating from offset
+         If iPad, and if button offsets passed, then show dilalog 
+         from offset
          */
-        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad &&
+         if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad &&
             dialogTopPos != 0 && dialogLeftPos != 0) {
             [controller presentFromRect:CGRectMake(self.dialogLeftPos, self.dialogTopPos, 0, 0) inView:self.webView animated:YES completionHandler:completionHandler];
-        } else {
-            [controller presentAnimated:YES completionHandler:completionHandler];
-        }
+    } else {
+        [controller presentAnimated:YES completionHandler:completionHandler];
     }
+}
 
 }
 
 -(BOOL) isPrintServiceAvailable{
-
+    
     Class myClass = NSClassFromString(@"UIPrintInteractionController");
     if (myClass) {
         UIPrintInteractionController *controller = [UIPrintInteractionController sharedPrintController];
         return (controller != nil) && [UIPrintInteractionController isPrintingAvailable];
     }
-
-
+    
+    
     return NO;
 }
 
